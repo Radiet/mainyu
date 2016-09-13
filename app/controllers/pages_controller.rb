52f1d99f905892b28c907add44179928e62525f7
Radiet Billahirabba <radiet@41studio.com>
2016-09-13 14:46:25 +0700

    Fix typo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def dashboard
     result = request.location
     if result.data['city'].present? && params[:location].nil?
-      @user_city = data['city']
+      @user_city = result.data['city']
     end
   end
 end
